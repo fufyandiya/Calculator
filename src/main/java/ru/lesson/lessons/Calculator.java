de4c326d9587289d1e13a4b6c01581a5b341c54c
Ivan <fufyandiya@gmail.com>
2019-07-15 23:52:44 +0400

    first commit

---
+++ src/main/java/ru/lesson/lessons/Calculator.java
@@ -0,0 +1,85 @@
+package ru.lesson.lessons;
+
+/**
+ * Класс, реализующий функции калькулятора
+ * @author Ivan Kachkin
+ * @version 1.1
+ */
+public class Calculator {
+	/**
+	 * Переменная, хранящая результат работы калькулятора
+	 */
+	private int result;
+
+	/**
+	 * Складывает две переменные
+	 * @param arg1
+	 * @param arg2
+	 * @result this.result
+	 */
+	public void add(final int arg1, final int arg2) {
+
+		this.result = arg1 + arg2;
+	}
+	/**
+	 * Вычитает две переменные
+	 * @param arg1
+	 * @param arg2
+	 * @result this.result
+	 */
+	public void subtract(final int arg1, final int arg2) {
+
+		this.result = arg1 - arg2;
+	}
+	/**
+	 * Умножает две переменные
+	 * @param arg1
+	 * @param arg2
+	 * @result this.result
+	 */
+	public void multiplicate(final int arg1, final int arg2) {
+
+		this.result = arg1 * arg2;
+	}
+	/**
+	 * Делит две переменные друг на друга
+	 * @param arg1
+	 * @param arg2
+	 * @result this.result
+	 */
+	public void divide(final int arg1, final int arg2) {
+		if (arg2 != 0)
+			this.result = arg1 / arg2;
+		else {
+			System.out.println("Second arg cannot be zero!");
+			this.result = 0;
+		}
+	}
+	/**
+	 * Возводит arg1 в степень arg2
+	 * @param arg1
+	 * @param arg2
+	 * @result this.result
+	 */
+	public void exponent(final int arg1, final int arg2) {
+		int x = 1;
+		for (int i = 0; i < arg2; i++) {
+			x *= arg1;
+		}
+		this.result = x;
+	}
+	/**
+	 * Возвращает результат работы калькулятора
+	 * @return this.result
+	 */
+	public int getResult() {
+		return this.result;
+	}
+	/**
+	 * Сбрасывает результат работы калькулятора
+	 * @return this.result
+	 */
+	public void cleanResult() {
+		this.result = 0;
+	}
+}
---
+++ src/main/java/ru/lesson/lessons/InteractRunner.java
@@ -0,0 +1,103 @@
+package ru.lesson.lessons;
+
+import java.util.Scanner;
+
+/**
+ * Класс, принимающий значения параметров и операций для калькулятора
+ * @author Ivan Kachkin
+ * @version 1.1
+ */
+public class InteractRunner {
+	static String first;
+	static String operation;
+	static String second;
+
+	public static void main (String[] arg) {
+		new InteractRunner().go();
+	}
+	/**
+	 * Создает входной поток и обьект класса Calculator
+	 */
+	public void go() {
+		Scanner reader = new Scanner(System.in);
+		try {
+			Calculator calc = new Calculator();
+			String exit = "нет";
+			/**
+			 * Принимает значения, пока в exit не будет "да"
+			 */
+			while (!exit.equals("да")) {
+
+				readFirst(calc, reader);
+				readOperation(reader);
+				readSecond(reader);
+
+				doOperation(calc, first, second, operation);
+				System.out.println("Результат: " + calc.getResult());
+				System.out.println("Выйти: Да/Нет ");
+				exit = reader.next().toLowerCase();
+			}
+		} finally {
+			reader.close();
+		}
+	}
+	/**
+	 * Заносит в first либо значение result из обьекта calc, либо входного потока reader
+	 * @param calc
+	 * @param reader
+	 */
+	public void readFirst(Calculator calc, Scanner reader) {
+		System.out.println("Использовать предыдущий результат в качестве первого аргумента? Да/Нет");
+		if (reader.next().toLowerCase().equals("да"))
+			first = String.valueOf(calc.getResult());
+		else {
+			calc.cleanResult();
+			System.out.println("Введите первый аргумент: ");
+			first = reader.next();
+		}
+	}
+	/**
+	 * Заносит в second значение входного потока reader
+	 * @param reader
+	 */
+	public void readSecond(Scanner reader) {
+		System.out.println("Введите второй аргумент: ");
+		second = reader.next();
+	}
+	/**
+	 * Заносит в operation значение входного потока reader
+	 * @param reader
+	 */
+	public void readOperation(Scanner reader) {
+		System.out.println("Введите операцию [+],[-],[*],[/],[^]: ");
+		operation = reader.next();
+	}
+
+	/**
+	 * Вызывает методы обьекта calc в зависимости от operation, либо
+	 * выводит выводит ошибку в консоль.
+	 * @param calc
+	 * @param firstNumber
+	 * @param secondNumber
+	 * @param operation
+	 */
+	public void doOperation(Calculator calc, String firstNumber, String secondNumber, String operation) {
+		if (operation.equals("+")) {
+			calc.add(Integer.valueOf(firstNumber), Integer.valueOf(secondNumber));
+		}
+		else if (operation.equals("-")) {
+			calc.subtract(Integer.valueOf(firstNumber), Integer.valueOf(secondNumber));
+		}
+		else if (operation.equals("*")) {
+			calc.multiplicate(Integer.valueOf(firstNumber), Integer.valueOf(secondNumber));
+		}
+		else if (operation.equals("/")) {
+			calc.divide(Integer.valueOf(firstNumber), Integer.valueOf(secondNumber));
+		}
+		else if (operation.equals("^")) {
+			calc.exponent(Integer.valueOf(firstNumber), Integer.valueOf(secondNumber));
+		}
+		else
+			System.out.println("Wrong operation!");
+	}
+}
